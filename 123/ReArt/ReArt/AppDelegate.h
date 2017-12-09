1952486298777409d2837107197d82df8176f294
xusea <xusea@xuseadeMacBook-Pro.local>
2017-12-09 12:10:54 +0800

    sss

---
+++ 123/ReArt/ReArt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ReArt
+//
+//  Created by xusea on 2017/12/9.
+//  Copyright © 2017年 TAOCHANG. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
